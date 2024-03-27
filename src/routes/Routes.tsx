d3b302f448b84b8e25aa5aaf318ae08eaa290e67
USER - 2020 <hackingetico2021@gmail.com>
2024-03-26 23:54:06 -0500

    feat: :truck: ADD ROUTES AN COMPONETNS

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,21 +3,22 @@ import Home from '../pages/Home.tsx';
 import NavBar from '../components/navbar/NavBar.tsx';
 import Footer from '../components/footer/Footer.tsx';
 import Login from '../components/auth/Login.tsx';
+import MediaControlCard from '../components/productsDetails/card.tsx';
 
 
 
 const AppRouter = () => {
     return (
         <>
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/prueba" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        <Footer />
-        </Router>
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/prueba" element={<MediaControlCard />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+                <Footer />
+            </Router>
         </>
 
     );
