b971032b44df3f11e6c7c325f8326618395c8075
marialu11 <mgaleano@unal.edu.co>
2024-04-16 15:03:53 -0500

    fix: styles

--- src/components/productsDetails/card.tsx
+++ src/components/productsDetails/card.tsx
@@ -10,17 +10,17 @@ export default function MediaControlCard() {
 
     return (
         <Grid container spacing={2}>
-            <Grid item xs={8}>
-                <Card sx={{ display: 'flex' }}>
+            <Grid item xs={12}>
+                <Card sx={{ display: 'flex', mt: 7, width: '100%', border:0 }}>
                     <CardMedia
                         component="img"
-                        sx={{ width: 300 }}
+                        sx={{ width: 500 }}
                         image="https://www.ceupe.com/images/easyblog_articles/3461/b2ap3_amp_productos-en-carrito-de-compra.jpg"
                         alt="Live from space album cover"
                     />                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                         <CardContent sx={{ flex: '1 0 auto' }}>
                             <Typography component="div" variant="h5">
-                                Live From Space
+                                Nombre del producto 
                             </Typography>
                             <Typography variant="subtitle1" color="text.secondary" component="div">
                                 Mac Miller Caracterisitcas del producto 
