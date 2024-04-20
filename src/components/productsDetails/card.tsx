af373693062a3a75bbf03377f7db2a2b46710ac4
marialu11 <mgaleano@unal.edu.co>
2024-04-19 23:55:33 -0500

    fix: styles

--- src/components/productsDetails/card.tsx
+++ src/components/productsDetails/card.tsx
@@ -11,19 +11,17 @@ export default function MediaControlCard() {
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <Card sx={{ display: 'flex', mt: 7, width: '100%', border:0 }}>
+                <Card variant="outlined" sx={{ display: 'flex', mx: 10, mt: 9, width: '100%', border: 0 }}>
                     <CardMedia
-                        component="img"
-                        sx={{ width: 500 }}
-                        image="https://www.ceupe.com/images/easyblog_articles/3461/b2ap3_amp_productos-en-carrito-de-compra.jpg"
-                        alt="Live from space album cover"
-                    />                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        component="img" sx={{ width: 500 }} image="https://www.ceupe.com/images/easyblog_articles/3461/b2ap3_amp_productos-en-carrito-de-compra.jpg"
+                        alt="Live from space album cover"/>                    
+                    <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
                         <CardContent sx={{ flex: '1 0 auto' }}>
                             <Typography component="div" variant="h5">
-                                Nombre del producto 
+                                Nombre del producto
                             </Typography>
                             <Typography variant="subtitle1" color="text.secondary" component="div">
-                                Mac Miller Caracterisitcas del producto 
+                                Mac Miller Caracterisitcas del producto
                             </Typography>
 
                         </CardContent>
