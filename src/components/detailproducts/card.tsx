74f9305f52f3b049f2b42c7957415eb21e6e4846
alondonologi <jzuluaga@logiseguridad.com>
2024-05-02 10:23:24 -0500

    feat: :art: Retrieve and display products from API
    This commit implements the functionality to fetch product data from the API and display it in the MediaControlCard component.  - Utilize axios library to make GET request to the API endpoint - Store the retrieved product data in the 'products' state variable - Render each product as a Card component with its name, description, and unit price - Implement loading indicator and error handling for better user experience

--- src/components/detailproducts/card.tsx
+++ src/components/detailproducts/card.tsx
@@ -1,43 +1,62 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Box, CircularProgress } from '@mui/material';
 
 export default function MediaControlCard() {
-    const [product, setProduct] = useState([]);
-    
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         axios.get('http://localhost:8080/api/products')
             .then((response) => {
-                console.table(response.data);
-                setProduct(response.data);
+                console.table(response.data.data); // Accede a la propiedad 'data' que contiene la matriz de productos
+                setProducts(response.data.data); // Almacena la matriz de productos en el estado 'products'
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                // @ts-ignore
+                setError('Error fetching data. Please try again later.');
+            })
+            .finally(() => {
+                setLoading(false);
             });
     }, []);
-    
-    return  (
-        <Grid container >
-            {/* <Grid item xs={12}>
-                {product && product.map((product) => (
-                    <Card key={product.id_producto}>
+
+
+    if (loading) {
+        return <CircularProgress />;
+    }
+
+    if (error) {
+        return <Typography variant="body1">{error}</Typography>;
+    }
+
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                {products.map(({ id_producto, product_name, description, unit_price }) => (
+                    <Card key={id_producto}>
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="div">
-                                {product.product_name}
+                                {product_name}
                             </Typography>
                             <Typography variant="body2" color="text.secondary">
-                                {product.description}
+                                {description}
                             </Typography>
                             <Box display="flex" justifyContent="flex-end">
                                 <Typography variant="body2" color="text.secondary">
-                                    {product.unit_price}
+                                    {unit_price}
                                 </Typography>
                             </Box>
                         </CardContent>
                     </Card>
                 ))}
-            </Grid> */}
-        </Grid >
-    ) ;
- }
+            </Grid>
+        </Grid>
+    );
+}
