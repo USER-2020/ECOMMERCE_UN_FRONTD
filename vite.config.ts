7db1d827e199173afe78cad4c66d499c0f5f279b
alondonologi <jzuluaga@logiseguridad.com>
2024-06-27 04:15:14 -0500

    pwa aplication icons add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        name: 'Ucommerce',
+        short_name: 'UCOM',
+        description: 'Aplicativo ecommerce universitario',
+        background_color: '#E3EAFE',
+        icons: [
+          {
+            "src": "/ucom192x192.png",
+            "type": "image/png",
+            "sizes": "192x144"
+          },
+          {
+            "src": "/ucom512x512.png",
+            "type": "image/png",
+            "sizes": "512x384"
+          }
+        ],
+      }
+    })
+
+  ],
 })
+
